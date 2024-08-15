79c46134c2d7a31376efd406fb5057dcb39d54cf
Dell <Dell@RohiniRK>
2024-08-15 15:00:30 +0530

    commit 1

---
+++ src/demo/script.java
@@ -0,0 +1,20 @@
+package demo;
+
+public class script {
+	static void sub(int no)
+	{
+	int fact=1;
+	for(int i=no;i>=1;i--)
+	{
+		fact=fact*i;
+	}
+	System.out.println(fact);
+	}
+
+	public static void main(String[] args) 
+	{
+		sub(8);
+		
+	}
+
+}
