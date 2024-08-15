f8cf3d688dff0186b583dd688ea686e6df7b23b5
Dell <Dell@RohiniRK>
2024-08-15 15:05:31 +0530

    committ

--- src/demo/script2.java
+++ src/demo/script2.java
@@ -1,5 +1,21 @@
 package demo;
 
 public class script2 {
+	static void sub(int no)
+	{
+	int fact=1;
+	for(int i=no;i>=1;i--)
+	{
+		fact=fact*i;
+	}
+	System.out.println(fact);
+	}
+
+	public static void main(String[] args) 
+	{
+		sub(8);
+		
+	}
+
 
 }
